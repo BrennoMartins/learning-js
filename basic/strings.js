90def373f937e9c0a4a6556bf8912f5b905e1fea
Brenno Martins <brennomartins@hotmail.com>
2025-08-09 23:43:56 -0300

    class 21,22,23

--- basic/strings.js
+++ basic/strings.js
@@ -1,3 +1,11 @@
 let umString  = 'Um "string"';
 
 //Parei no video 
+
+let text = "Um texto";
+
+console.log(text.charAt(2));
+console.log(text.replace('Um', 'Dois'))
+console.log(text,slice(1,2)); 
+
+//20:00
