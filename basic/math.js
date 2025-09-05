f346f4cb2cabc928640b3abbddab3ef478929411
Brenno Martins <brennomartins@hotmail.com>
2025-09-05 20:03:15 -0300

    math

---
+++ basic/math.js
@@ -0,0 +1,6 @@
+let num1 = 9.54578;
+let num2 = Math.ceil(num1);
+console.log(num2);
+console.log(Math.max(1,2,3,4,5))
+// Numero Aleatorio ente 5 - 10
+console.log(Math.round(Math.random() * (10 - 5) + 5))
